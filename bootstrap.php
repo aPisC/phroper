200e1511a29e70c0c67b5309b26c6ef989121dd1
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-07-21 13:12:51 +0200

    Add bootstrap php

---
+++ bootstrap.php
@@ -0,0 +1,26 @@
+<?php 
+  require_once('utils/functions.php');
+
+  // Register auto loaging function
+  spl_autoload_register(function ($name) {
+    $name = str_replace('\\', DS, $name);
+
+    // Load file directly
+    if(file_exists($name . '.php'))require_once(file_exists($name . '.php'));
+    else if(file_exists($name . DS . 'index.php')) require_once($name . DS . 'index.php');
+
+    // Load core module
+    else if (file_exists('core' . DS . $name . '.php')) require_once('core' . DS . $name . '.php');
+    else if (file_exists('core' . DS . $name . DS . 'index.php')) require_once('core' . DS . $name . DS . 'index.php');
+
+    else echo 'unable to load: ' . 'core' . DS . $name . '.php';
+
+  });
+  
+
+  $router = new Router();
+
+  $router->addNamspace('api/:controller', 'Routers\ApiRouter');
+
+  $router->run($url);
+?>

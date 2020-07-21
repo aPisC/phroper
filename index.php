fdfd69b466e17587591bee26c5965b6fbb0af571
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-07-21 13:13:08 +0200

    Fix url trimming

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   define('ROOT', dirname(__FILE__));
   define('DS', DIRECTORY_SEPARATOR);
   
-  $url = isset($_GET['url']) ? explode('/', ltrim($_GET['url'], '/')) : [];
+  $url = isset($_GET['url']) ? explode('/', trim($_GET['url'], '/')) : [];
 
   require_once(ROOT . DS . 'bootstrap.php');
 ?>

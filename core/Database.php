69a67bd8288a1bd588f50181f1ed50ac0c326e95
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-07-22 13:46:12 +0200

    Create database singleton class

---
+++ core/Database.php
@@ -0,0 +1,19 @@
+<?php
+  class Database{
+    private static $config = array(
+      'user' => 'bendeguz',
+      'password' => 'sqlpas',
+      'server' => 'localhost',
+      'port' => null,
+      'database' => 'test'
+    );
+
+    private static ?mysqli $_instance = null;
+    public static function instance(){
+      if(self::$_instance == null)
+        self::$_instance = new mysqli(self::$config['server'], self::$config['user'], self::$config['password'], self::$config['database'], self::$config['port'] );
+      return self::$_instance;
+    }
+  }
+
+?>

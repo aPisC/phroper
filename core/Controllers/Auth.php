6a95ac316c1c3f5d2f6850cfca0f0855a5ff5364
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-07-23 18:38:01 +0200

    Add register fn to auth service

--- core/Controllers/Auth.php
+++ core/Controllers/Auth.php
@@ -4,6 +4,7 @@ namespace Controllers;
 use Controller;
 use Service;
 use Services\Auth as AuthService;
+use Context;
 class Auth extends Controller{
   private AuthService $service;
   
@@ -12,12 +13,18 @@ class Auth extends Controller{
 
     $this->service = Service::getService('Auth');
 
-    $this->registerJsonHandler("me", function($u, $params) {return $params['user'];});
+    $this->registerJsonHandler("me", function($u, $params) {return Context::get('user'); });
     $this->registerJsonHandler("login", function() {return $this->login();}, 'POST');
+    $this->registerJsonHandler("register", function() {return $this->register();}, 'POST');
   }
 
   public function login(){
     $data = json_load_body();
     return $this->service->login($data['username'], $data['password']);
+  }
+
+  public function register(){
+    $data = json_load_body();
+    return $this->service->register($data['username'], $data['email'], $data['password']);
   }
 }

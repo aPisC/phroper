f194b2c959aa340e240cfc7628851b3128314acf
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-07-23 18:36:31 +0200

    Add permission check to controller json handlers

--- core/Controller.php
+++ core/Controller.php
@@ -21,15 +21,30 @@ class Controller {
     $this->router->run($u, $p);
   }
 
+  protected function havePermission($action, $throw = false){
+    $user = Context::get('user');
+    $auth = Service::getService('Auth');
+    $permName = strtolower(str_replace('\\', '_', get_class($this) ) . '_' . $action);
+
+    $have = $auth->havePermission($user, $permName);
+    if(!$have && $throw)
+      throw new Exception('User has no permission to use ' . $permName, 403);
+    return $have;
+  }
+
   protected function registerHandler($name,$fun,$method = 'GET'){
     $this->router->add($name, $fun, $method);
   }
 
   protected function registerJsonHandler($name,$fun,$method = 'GET'){
 
-    $this->router->add($name, function($url, $params) use ($fun){
+    $this->router->add($name, function($url, $params) use ($fun, $name){
       header('Content-Type: application/json');
       try{
+        // Throwing exception when user has no permission
+        var_dump(strtolower($params['method'] . '_' . $name));
+        $this->havePermission(strtolower($params['method'] . '_' . $name), true);
+
         $result = $fun($url, $params);
         if($result == null){
           http_response_code(404);

b607f40754875d7f8e5c50fdef7214653d7ddb28
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-03-05 17:42:33 +0100

    Remove updated_by field from auth_user model

--- core/Models/Auth_User.php
+++ core/Models/Auth_User.php
@@ -8,8 +8,10 @@ class Auth_User extends Model {
   public function __construct() {
     parent::__construct('user');
 
+    $this->fields['updated_by'] = null;
+
     $this->fields["username"] = new Model\Fields\Text();
-    $this->fields["role"] = new Model\Fields\RelationToOne("Auth_Role", ["field" => "role_id"]);
+    $this->fields["role"] = new Model\Fields\RelationToOne("Auth_Role");
     $this->fields["password"] = new Model\Fields\Password();
     $this->fields["email"] = new Model\Fields\Text();
   }

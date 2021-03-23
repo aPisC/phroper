3c4d961bf98d099305d12025e3ad44f054079948
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-03-23 10:52:31 +0100

    remove Id from Store model

--- phapi/Models/Store.php
+++ phapi/Models/Store.php
@@ -8,7 +8,8 @@ class Store extends Model {
   public function __construct() {
     parent::__construct('store');
 
-    $this->fields["key"] = new Model\Fields\Text();
+    $this->fields = [];
+    $this->fields["key"] = new Model\Fields\TextKey();
     $this->fields["value"] = new Model\Fields\Json();
   }
 

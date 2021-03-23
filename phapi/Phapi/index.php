e63915e739a5156d7a902ebb3bede822a1773bab
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-03-23 12:21:58 +0100

    Add index.php to handle importing

--- phapi/Phapi/index.php
+++ phapi/Phapi/index.php
@@ -1,32 +1,6 @@
 <?php
 
-// Defines
-
 use Phapi\Phapi_instance;
-
-if (!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
-define('PHAPI_VERSION', '1.1.0');
-
-// Register auto loaging function
-spl_autoload_register(function ($name) {
-    $name = str_replace('\\', DS, $name);
-
-    // Load file directly
-    if (file_exists($name . '.php')) require_once($name . '.php');
-    else if (file_exists($name . DS . 'index.php')) require_once($name . DS . 'index.php');
-
-    // Load core module
-    else if (file_exists('phapi' . DS . $name . '.php')) require_once('phapi' . DS . $name . '.php');
-    else if (file_exists('phapi' . DS . $name . DS . 'index.php')) require_once('phapi' . DS . $name . DS . 'index.php');
-});
-
-// Global imports
-require_once(ROOT . DS . 'phapi' . DS . 'Utils' . DS . 'functions.php');
-
-
-/* ------------------
-Phapi singleton wrapper
------------------- */
 
 class Phapi {
     // Singleton factory

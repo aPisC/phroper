04f838fc2978fb9cb580e157942774078a7a170f
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-04-28 11:15:30 +0200

    Hide System models on ui

--- src/Models/FileUpload.php
+++ src/Models/FileUpload.php
@@ -7,7 +7,7 @@ use Phroper\Fields\Text;
 
 class FileUpload extends Model {
     public function __construct() {
-        parent::__construct();
+        parent::__construct(["visible" => false]);
 
         $this->fields["updated_at"] = null;
         $this->fields["filename"] = new Text();
--- src/Models/Store.php
+++ src/Models/Store.php
@@ -8,7 +8,7 @@ use Phroper\Model;
 
 class Store extends Model {
   public function __construct() {
-    parent::__construct(["table" => "store"]);
+    parent::__construct(["table" => "store", "visible" => false]);
 
     $this->fields->clear();
     $this->fields["key"] = new TextKey();

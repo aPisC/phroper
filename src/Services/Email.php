518c80148f6e5ef50769a82e233a9d661dc4d8b2
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-04-28 11:20:59 +0200

    Add logging to email service

--- src/Services/Email.php
+++ src/Services/Email.php
@@ -7,6 +7,7 @@ use Phroper\Service;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+use Throwable;
 
 class Email extends Service {
     protected function getTemplatePath($template) {
@@ -21,9 +22,6 @@ class Email extends Service {
     public function processTemplate($template, $data = []) {
         $path = $this->getTemplatePath($template);
         if (!$path) {
-            Phroper::service("log")->error(
-                "Email template not found: " . $template
-            );
             throw new Exception("Email template not found: " . $template);
         }
 
@@ -43,20 +41,30 @@ class Email extends Service {
     }
 
     public function send($template, $address, $data = []) {
-        // Initialize mailer
-        $mail = Phroper::ini("MAIL");
-        if (is_callable($mail)) $mail = $mail();
+        try {
+            // Initialize mailer
+            $mail = Phroper::ini("MAIL");
+            if (is_callable($mail)) $mail = $mail();
 
-        $mail->addAddress($address);
+            $mail->addAddress($address);
 
-        // Process body
-        $this->currentMail = $mail;
-        $body = $this->processTemplate($template, $data);
-        $this->currentMail = null;
-        $mail->Body = $body;
+            // Process body
+            $this->currentMail = $mail;
+            $body = $this->processTemplate($template, $data);
+            $this->currentMail = null;
+            $mail->Body = $body;
 
-        // Send mail
-        $mail->send();
+            // Send mail
+            $mail->send();
+        } catch (Throwable $e) {
+            Phroper::service("log")->error(
+                "Email sending error (" . $template . ", " . $address . ")\n" . $e
+            );
+            throw $e;
+        }
+        Phroper::service("log")->info(
+            "Email sent (" . $template . ", " . $address . ")"
+        );
 
         return true;
     }

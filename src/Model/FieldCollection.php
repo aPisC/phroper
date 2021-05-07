3e043358f0a66db2fc741dd4259793566887d15f
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-05-07 09:54:23 +0200

    Add types to fieldCollection

--- src/Model/FieldCollection.php
+++ src/Model/FieldCollection.php
@@ -3,9 +3,11 @@
 namespace Phroper\Model;
 
 use ArrayAccess;
+use ArrayIterator;
 use ArrayObject;
 use Exception;
 use IteratorAggregate;
+use Phroper\Fields\Field;
 use Phroper\Model;
 
 class FieldCollection implements ArrayAccess, IteratorAggregate {
@@ -16,14 +18,15 @@ class FieldCollection implements ArrayAccess, IteratorAggregate {
         $this->model = $model;
     }
 
-    public function addInternalField($key, $field) {
+    public function addInternalField(string $key, Field $field) {
         $this->fields[$key] = $field;
         $field->bindModel($this->model, $key);
     }
 
-    public function offsetSet($offset, $field) {
-        if (strpos($offset, ".") !== false)
-            throw new Exception("Field name " . $offset . " can not contain dots.");
+    public function offsetSet($offset, $field): void {
+        if (!is_string($offset)) throw new Exception("Field name must be string");
+        if ($field && !($field instanceof Field)) throw new Exception("Field must be inherited from Phroper\\Field");
+        if (strpos($offset, ".") !== false) throw new Exception("Field name " . $offset . " can not contain dots.");
 
         if (!$field) {
             unset($this->fields[$offset]);
@@ -34,30 +37,30 @@ class FieldCollection implements ArrayAccess, IteratorAggregate {
         $field->bindModel($this->model, $offset);
     }
 
-    public function offsetExists($offset) {
+    public function offsetExists($offset): bool {
         return array_key_exists($offset, $this->fields);
     }
 
-    public function offsetUnset($offset) {
+    public function offsetUnset($offset): void {
         unset($this->fields[$offset]);
     }
 
-    public function offsetGet($offset) {
+    public function offsetGet($offset): Field {
         if (isset($this->fields[$offset]))
             return $this->fields[$offset];
         return null;
     }
 
-    public function clear() {
+    public function clear(): void {
         $this->fields = [];
     }
 
-    public function keys() {
+    public function keys(): array {
         return array_keys($this->fields);
     }
 
 
-    public function getIterator() {
+    public function getIterator(): ArrayIterator {
         return (new ArrayObject($this->fields))->getIterator();
     }
 }

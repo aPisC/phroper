6451be0047d0a71e0e8ff5685b3f249701bc6864
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-05-23 18:48:13 +0200

    change templateData type on email

--- src/Services/Email.php
+++ src/Services/Email.php
@@ -77,7 +77,7 @@ class Email extends Service {
         return false;
     }
 
-    private array $templateData = [];
+    private $templateData = [];
     public function getTemplateData() {
         return $this->templateData;
     }

fd53ba4de526ec1353c20743bc3feee8c635405f
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-05-24 10:04:33 +0200

    set primary and display in store model

--- src/Models/Store.php
+++ src/Models/Store.php
@@ -8,7 +8,12 @@ use Phroper\Model;
 
 class Store extends Model {
   public function __construct() {
-    parent::__construct(["table" => "store", "visible" => false]);
+    parent::__construct([
+      "table" => "store",
+      "visible" => false,
+      "primary" => "key",
+      "display" => "key",
+    ]);
 
     $this->fields->clear();
     $this->fields["key"] = new TextKey();
